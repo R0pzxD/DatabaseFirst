f48108ec1091ce18b101aabf52715052a54d4427
R0pz <R0pz@DESKTOP-NS06MOG>
2025-10-01 18:37:47 +0300

    database first

--- DatabaseTask/DatabaseTask/Program.cs
+++ DatabaseTask/DatabaseTask/Program.cs
@@ -1,4 +1,4 @@
-using DatabaseTask.Data;
+//using DatabaseTask.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
@@ -7,8 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<DatabaseTaskDbContext>(options => 
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.AddDbContext<DatabaseTaskDbContext>(options => 
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 //void ConfigureServices(IServiceCollection services)
 //{
 //    services.AddDbContext<DatabaseTaskDbContext>(options =>
