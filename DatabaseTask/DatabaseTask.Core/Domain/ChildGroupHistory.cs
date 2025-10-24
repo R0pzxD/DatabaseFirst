1dc6dfe334ff10436d7e47ad77fd9c3b12689c00
hazeoffical <160328838+hazeoffical@users.noreply.github.com>
2025-10-24 15:57:27 +0300

    1

---
+++ DatabaseTask/DatabaseTask.Core/Domain/ChildGroupHistory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseTask.Core.Domain
+{
+    class ChildGroupHistory
+    {
+        [Key]
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Guid GroupId { get; set; }
+        public Guid ChildId { get; set; }
+    }
+}
---
+++ DatabaseTask/DatabaseTask.Data/DatabaseTaskDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace DatabaseTask.Data
+{
+    class DatabaseTaskDbContext : DbContext
+    {
+        public class DatabaseTaskDbContext(DbContextOptions<DatabaseTaskDbContext> options)
+            : base(options){}
+
+    public DbSet<Employee> Employees { get; set; }
+    }
+}
